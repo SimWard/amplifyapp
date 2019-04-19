3b019b05edd99e6da5b194a52841f9eed69865c1
Ryan Ward <wardyryan1@gmail.com>
2019-04-19 15:35:37 +1000

    Test changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hope this works and also I do not yet know how else to do anything in ReactJS.
           </p>
           <a
             className="App-link"
